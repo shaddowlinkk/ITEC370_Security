d5c0b5345dad4e410c05d64304e37198815ff2cf
shaddowlinkk <nathan.vanbrunt@gmail.com>
2018-02-22 12:55:57 -0500

    fixing mistakes

--- Login.php
+++ Login.php
@@ -47,7 +47,7 @@
 				//Upon receiving information form the submit validates information then runs it against the info array
 				if ($_SERVER["REQUEST_METHOD"] == "POST") 
 				{
-					$username = hash('sha256',check_input($_POST['username']))};
+					$username = hash('sha256',check_input($_POST['username']));
 					$password = check_input($_POST['password']);
 					echo $username;
 					
