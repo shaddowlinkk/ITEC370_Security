c2b72b0e59ee673e3964e21c1a0967770517c53d
Hope <36671425+Hope-Sofia@users.noreply.github.com>
2018-02-27 13:02:27 -0500

    Made things look pretty
    This uses the code provided by Nathan Alpha. With some modifications and of course still making sure it connects to the SQL database.

--- AccountPage.php
+++ AccountPage.php
@@ -1,23 +1,33 @@
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>I.S.M - Acount Page</title>
-		
-		<link rel="stylesheet" type="text/css" href="CSS/Style.css">
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  
+	  <title>I.S.M - Acount Page</title>
+	  
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+	  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	  
+	  <!-- Bulma Version 0.6.0 -->
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css" integrity="sha256-HEtF7HLJZSC3Le1HcsWbz1hDYFPZCqDhZa9QsCgVUdw=" crossorigin="anonymous" />
+	  <link rel="stylesheet" type="text/css" href="../css/login.css">
 	</head>
 	
-	<body> 
-		<!--This is for the header and navigation-->
-		<div>
-		</div>
-		
-		<!--Main body of stuff-->
-		<div class="box">
-			<div class="content">
-				Account of: 
+	<body bgcolor="ffffff">
+	  <section >
+		<div class="hero-body">
+		  <div class="container has-text-centered">
+			<div class="column is-4 is-offset-4">
+			  Account of: 
 				<?php session_start();
 					echo $_SESSION['login'];
-				?>			
+				?>
 			</div>
+		  </div>
 		</div>
+	  </section>
+	  <script async type="text/javascript" src="../js/bulma.js"></script>
 	</body>
 </html>
--- Login.php
+++ Login.php
@@ -1,86 +1,115 @@
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>I.S.M. - Login</title>
-		
-		<link rel="stylesheet" type="text/css" href="CSS/Style.css">
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  
+	  <title>I.S.M. - Login</title>
+	  
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+	  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	  
+	  <!-- Bulma Version 0.6.0 -->
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css" integrity="sha256-HEtF7HLJZSC3Le1HcsWbz1hDYFPZCqDhZa9QsCgVUdw=" crossorigin="anonymous" />
+	  <link rel="stylesheet" type="text/css" href="../css/login.css">
 	</head>
 	
-	<body> 
-		<!--This is for the header and navigation-->
-		<div>
-		</div>
-		<!--Main body of stuff-->
-		<div class="box">
-			<div class="content">
-			<?php
-				$username = $password = "";
+	<body bgcolor="ffffff">
+	  <?php
+			$username = $password = "";
+			
+			session_start();
+			
+			//Used later for pulling up the information for an account
+			$_SESSION['login'] = null;
+			$loginCorrect = false;
+			//Upon receiving information form the submit validates information then runs it against the info array
+			if ($_SERVER["REQUEST_METHOD"] == "POST") 
+			{
+				$username = check_input($_POST['username']);
+				$password = hash('sha256',check_input($_POST['password']));
 				
-				session_start();
+				//connects to the database for users
+				$conn = new mysqli("localhost", "itec370", "itec", "itec370");
+			
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				} 
 				
-				echo "<form action=";
-				echo htmlspecialchars($_SERVER["PHP_SELF"]);
-				echo" method='post'>
-				Username: <input type='text' name='username' class='validate' pattern='[a-z]{1,}'> <br><br>
-				Password: <input type='password' name='password' class='validate' pattern='[A-z]{1,}'><br>";
-				if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+				$query = "SELECT password, UID FROM login WHERE username= '".$username."'";
+				$userlist = $conn->query($query);
+				
+				if ($userlist->num_rows > 0) 
+				{
+					// output data of each row
+					while($row = $userlist->fetch_assoc())
+					{
+						if ($password == $row["password"])
+						{
+							$_SESSION['login'] = $row["UID"];
+							$loginCorrect = true;
+							echo "<script>location.href='AccountPage.php';</script>";
+						}
+					}
+				}
+				
+				$conn->close();
+			}
+			
+			//Used for validating data No Hackers allowed (Hopefully)
+			function check_input ($data)
+			{
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+			}
+		?>
+	  <section >
+		<div class="hero-body">
+		  <div class="container has-text-centered">
+			<div class="column is-4 is-offset-4">
+			  <div class="box">
+			  
+				<figure class="avatar">
+				  <img src="https://placehold.it/128x128">
+				</figure>
+				<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+				
+				  <div class="field">
+					<div class="control">
+					  <input class="input is-large" type="username" name="username" placeholder="User Name" autofocus="">
+					</div>
+				  </div>
+
+				  <div class="field">
+					<div class="control">
+					  <input class="input is-large" type="password" name="password" placeholder="Password">
+					</div>
+				  </div>
+				  
+				  <?php 
+					if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 					{
 						if (!$loginCorrect) 
 						{
 							echo "<span style='color: red;'>Invalid login information, please try again.</span>";
 						}
-				}
-				echo "<br><input type='submit'><br>";
-				
-				//Used later for pulling up the information for an account
-				$_SESSION['login'] = null;
-				$loginCorrect = false;
-				//Upon receiving information form the submit validates information then runs it against the info array
-				if ($_SERVER["REQUEST_METHOD"] == "POST") 
-				{
-					$username = check_input($_POST['username']);
-					$password = hash('sha256',check_input($_POST['password']));
-					
-					//connects to the database for users
-					$conn = new mysqli("localhost", "itec370", "itec", "itec370");
-				
-					// Check connection
-					if ($conn->connect_error) {
-						die("Connection failed: " . $conn->connect_error);
-					} 
-					
-					$query = "SELECT password, UID FROM login WHERE username= '".$username."'";
-					$userlist = $conn->query($query);
-					
-					if ($userlist->num_rows > 0) 
-					{
-						// output data of each row
-						while($row = $userlist->fetch_assoc())
-						{
-							if ($password == $row["password"])
-							{
-								$_SESSION['login'] = $row["UID"];
-								$loginCorrect = true;
-								echo "<script>location.href='AccountPage.php';</script>";
-							}
-						}
 					}
+				  ?>
+				  
+				  <input type="submit" value="Login" class="button is-block is-info is-large is-fullwidth">
+				</form>
 					
-					$conn->close();
-				}
 				
-				//Used for validating data No Hackers allowed (Hopefully)
-				function check_input ($data)
-				{
-					$data = trim($data);
-					$data = stripslashes($data);
-					$data = htmlspecialchars($data);
-					return $data;
-				}
-			?>
-			<!--This is the HTML for the form that the user provides input for -->
-			
-			</form>
+				
+			  </div>
+			</div>
+		  </div>
 		</div>
-		</div>
+	  </section>
+	  <script async type="text/javascript" src="../js/bulma.js"></script>
 	</body>
 </html>
