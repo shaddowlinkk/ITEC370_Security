e05f6df26f29067f53463eecb065c96f77c2367d
Hope <36671425+Hope-Sofia@users.noreply.github.com>
2018-04-05 12:59:22 -0400

    Fixed some stuff that was not needed
    It still works

--- AccountPage.php
+++ AccountPage.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	  <meta charset="utf-8">
-	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	  <meta name="viewport" content="width=device-width, initial-scale=1">
-	  
-	  
-	  <title>I.S.M - Acount Page</title>
+	 <title>I.S.M - Acount Page</title>
 	  
 	<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
 	 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
